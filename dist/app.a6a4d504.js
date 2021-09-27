c5b4c8a11897b7545078ed4536b24389e15aec41
Ben <j1712032377@163.com>
2021-09-27 13:28:01 +0800

    添加 icon 和 iconPosition 属性

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12487,7 +12487,14 @@ exports.default = void 0;
 //
 //
 //
-var _default = {};
+//
+//
+//
+//
+//
+var _default = {
+  props: ['icon', 'iconPosition']
+};
 exports.default = _default;
         var $16c1f8 = exports.default || module.exports;
       
@@ -12498,10 +12505,27 @@ exports.default = _default;
         /* template */
         Object.assign($16c1f8, (function () {
           var render = function() {
+  var _obj
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("button", { staticClass: "t-button" }, [_vm._v("按钮")])
+  return _c(
+    "button",
+    {
+      staticClass: "t-button",
+      class: ((_obj = {}), (_obj["icon-" + _vm.iconPosition] = true), _obj)
+    },
+    [
+      _vm.icon
+        ? _c("svg", { staticClass: "icon" }, [
+            _c("use", { attrs: { "xlink:href": "#i-" + _vm.icon } })
+          ])
+        : _vm._e(),
+      _vm._v(" "),
+      _vm._t("default")
+    ],
+    2
+  )
 }
 var staticRenderFns = []
 render._withStripped = true
@@ -12578,7 +12602,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62326" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60555" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/button.vue
+++ src/button.vue
@@ -1,27 +1,53 @@
 <template>
-  <button class="t-button">按钮</button>
+  <button class="t-button" :class="{[`icon-${iconPosition}`]:true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#i-${icon}`></use>
+    </svg>
+      <slot></slot>
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['icon', 'iconPosition']
+}
 </script>
 
 <style lang="scss" scoped>
 .t-button {
+  vertical-align: middle;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
   font-size: var(--font-size);
   height: var(--button-height);
   padding: 0 1em;
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
-  &:hover{
+
+  &:hover {
     border-color: var(--border-coloc-hover);
   }
-  &:active{
+
+  &:active {
     background-color: var(--button-active-bg);
   }
-  &:focus{
+
+  &:focus {
     outline: none;
+  }
+
+  > .icon{
+    margin-right: .1em;
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .1em;
+    }
   }
 }
 </style>
