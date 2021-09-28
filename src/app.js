0bd8591655ab8ad42e91c07271faa39fa562774f
Ben <j1712032377@163.com>
2021-09-28 22:04:28 +0800

    使用 mocha 和 karma 测试失败

--- src/app.js
+++ src/app.js
@@ -16,86 +16,87 @@ new Vue({
     }
 })
 
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect=chai.expect
-{
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:"good"
-        }
-    })
-    vm.$mount()
-    let useElement=vm.$el.querySelector('use')
-    expect(useElement.getAttribute("xlink:href")).to.eq('#i-good')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:"setting",
-            loading:true
-        }
-    }).$mount()
-    let useElement=vm.$el.querySelector('use')
-    expect(useElement.getAttribute("xlink:href")).to.eq('#i-loading')
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:"setting"
-        }
-    })
-    vm.$mount(div)
-    let svg=vm.$el.querySelector('svg')
-    let order=window.getComputedStyle(svg).order
-    expect(order).to.eq("-1")
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:"setting",
-            iconPosition:"right"
-        }
-    })
-    vm.$mount(div)
-    let svg=vm.$el.querySelector('svg')
-    let order=window.getComputedStyle(svg).order
-    expect(order).to.eq("2")
-    vm.$el.remove()
-    vm.$destroy()
-}
-{
-    // mock
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const vm=new Constructor({
-        propsData:{
-            icon:"setting",
-            iconPosition:"right"
-        }
-    })
-    vm.$mount(div)
-    let spy=chai.spy(function(){})
-    vm.$on('click',spy)
-    let button=vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
-    vm.$el.remove()
-    vm.$destroy()
-}
+// 第一次测试
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect=chai.expect
+// {
+//     const Constructor=Vue.extend(Button)
+//     const vm=new Constructor({
+//         propsData:{
+//             icon:"good"
+//         }
+//     })
+//     vm.$mount()
+//     let useElement=vm.$el.querySelector('use')
+//     expect(useElement.getAttribute("xlink:href")).to.eq('#i-good')
+//     vm.$el.remove()
+//     vm.$destroy()
+// }
+// {
+//     const Constructor=Vue.extend(Button)
+//     const vm=new Constructor({
+//         propsData:{
+//             icon:"setting",
+//             loading:true
+//         }
+//     }).$mount()
+//     let useElement=vm.$el.querySelector('use')
+//     expect(useElement.getAttribute("xlink:href")).to.eq('#i-loading')
+//     vm.$el.remove()
+//     vm.$destroy()
+// }
+// {
+//     const div=document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor=Vue.extend(Button)
+//     const vm=new Constructor({
+//         propsData:{
+//             icon:"setting"
+//         }
+//     })
+//     vm.$mount(div)
+//     let svg=vm.$el.querySelector('svg')
+//     let order=window.getComputedStyle(svg).order
+//     expect(order).to.eq("-1")
+//     vm.$el.remove()
+//     vm.$destroy()
+// }
+// {
+//     const div=document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor=Vue.extend(Button)
+//     const vm=new Constructor({
+//         propsData:{
+//             icon:"setting",
+//             iconPosition:"right"
+//         }
+//     })
+//     vm.$mount(div)
+//     let svg=vm.$el.querySelector('svg')
+//     let order=window.getComputedStyle(svg).order
+//     expect(order).to.eq("2")
+//     vm.$el.remove()
+//     vm.$destroy()
+// }
+// {
+//     // mock
+//     const div=document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor=Vue.extend(Button)
+//     const vm=new Constructor({
+//         propsData:{
+//             icon:"setting",
+//             iconPosition:"right"
+//         }
+//     })
+//     vm.$mount(div)
+//     let spy=chai.spy(function(){})
+//     vm.$on('click',spy)
+//     let button=vm.$el
+//     button.click()
+//     expect(spy).to.have.been.called()
+//     vm.$el.remove()
+//     vm.$destroy()
+// }
